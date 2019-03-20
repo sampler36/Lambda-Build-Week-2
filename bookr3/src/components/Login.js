8f3225ec4f5791721adc05585399f33c9e59ed9b
Talent Antonio <antonio.talenty@gmail.com>
2019-03-20 00:22:04 +0000

    Styling Update- Materialize

--- bookr3/src/components/Login.js
+++ bookr3/src/components/Login.js
@@ -19,12 +19,23 @@ export class Login extends React.Component {
     return (
       <div className="log">
         <h3>Login</h3>
-        <div>username <input type="text" ref={this.userRef} /></div>
-        <div>password <input type="text" ref={this.passRef} /></div>
+        {/* <div>username <input type="text" ref={this.userRef} /></div> */}
+        {/* <div>password <input type="text" ref={this.passRef} /></div> */}
 
+        <div class="row">
+        <div class="input-field col s12">
+          <input id="email" type="email" class="validate" ref={this.userRef}   />
+          <label for="email">Username</label>
+        </div>
+
+        <div className="input-field col s12 color-white">
+          <input id="password" type="password" class="validate" ref={this.passRef} />
+          <label for="password">Password</label>
+        </div>
         <button onClick={this.onLogin}>Log in</button>
         {/* Create a Log Out button that flushes 'userToken' from local storage */}
         <button onClick={() => localStorage.clear()}>Log Out</button>
+      </div>
       </div>
     );
   }
